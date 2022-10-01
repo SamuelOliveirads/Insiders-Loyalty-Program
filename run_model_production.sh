7908fa64301dc114f05fb382ca7e56ba3b19f5e9
samuel <samueloliveira32df@gmail.com>
2022-10-01 17:58:21 -0300

    add path from papermill

--- run_model_production.sh
+++ run_model_production.sh
@@ -3,6 +3,6 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #_ path 
 path='/home/ubuntu/insiders_clustering'
-path_to_envs='/home/ubuntu/.local/bin/'
+path_to_envs='/home/ubuntu/.pyenv/shims'
 
 $path_to_envs/papermill $path/src/models/c7.2-soaf-deploy_aws.ipynb $path/reports/c7.2-soaf-deploy_aws-$data.ipynb
