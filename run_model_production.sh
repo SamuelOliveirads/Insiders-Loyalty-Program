ab90a545bd7f5e0f5c5964e93e71c5810a1de5d1
samuel <samueloliveira32df@gmail.com>
2022-10-01 18:11:59 -0300

    update path from aws and final model to production

--- run_model_production.sh
+++ run_model_production.sh
@@ -2,7 +2,7 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #_ path 
-path='/home/ubuntu/insiders_clustering'
+path='/home/ubuntu/Insiders-Loyalty-Program'
 path_to_envs='/home/ubuntu/.pyenv/shims'
 
-$path_to_envs/papermill $path/src/models/c7.2-soaf-deploy_aws.ipynb $path/reports/c7.2-soaf-deploy_aws-$data.ipynb
+$path_to_envs/papermill $path/src/models/c7.1-soaf-deploy.ipynb $path/reports/c7.1-soaf-deploy-$data.ipynb
