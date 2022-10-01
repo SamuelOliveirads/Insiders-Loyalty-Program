87ec6345593c115ea2d9c7257bf1e3cf0d307d6d
samuel <samueloliveira32df@gmail.com>
2022-10-01 17:53:48 -0300

    modified path from cromjob

--- run_model_production.sh
+++ run_model_production.sh
@@ -2,7 +2,7 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #_ path 
-path='/home/ubuntu/pa005_insiders_clustering/insiders_clustering'
+path='/home/ubuntu/insiders_clustering'
 path_to_envs='/home/ubuntu/.local/bin/'
 
-$path_to_envs/papermill $path/src/models/c7.0-soaf-deploy.ipynb $path/reports/c7.0-soaf-deploy-$data.ipynb
+$path_to_envs/papermill $path/src/models/c7.2-soaf-deploy_aws.ipynb $path/reports/c7.2-soaf-deploy_aws-$data.ipynb
